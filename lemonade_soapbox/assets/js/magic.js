2045788085f07c3e4f382b0043ddd67e8ef2a32c
Ben Babcock <ben@tachyondecay.net>
2018-09-12 20:08:36 -0400

    Blog archive sidebar only shows two most recent years

--- lemonade_soapbox/assets/js/magic.js
+++ lemonade_soapbox/assets/js/magic.js
@@ -1,12 +1,20 @@
 $(function() {
-    $('<a>')
-        .addClass('expand')
-        .text('▼')
-        .appendTo('.expandable')
-        .click(function(e) {
-            $(this).parent().siblings('.collapsible').slideToggle();
-            currText = $(this).text();
-            $(this).text((currText == '▼') ? '▲' : '▼');
-        });
-    $('.collapsible').hide();
+    var years = $('.c-calendar-nav__item').length - 2;
+    $('<li>')
+        .addClass('c-calendar-nav__more')
+        .append(
+            $('<a>')
+                .addClass('c-btn')
+                .text('Show ' + years + ' more years')
+                .attr('href', '#')
+                .click(function(e) {
+                    var self = this;
+                    $(this).fadeOut('fast', function() {
+                        $(self).parent().siblings().fadeIn('fast');
+                    });
+                    e.preventDefault();
+                })
+        )
+        .insertAfter('.c-calendar-nav__item:eq(1)');
+    $('.c-calendar-nav__item:gt(1)').hide();
 });
