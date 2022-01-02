88c2e1654f1160877c7011b1df23a6538568a7fc
Kara Babcock <kara@tachyondecay.net>
2022-01-02 15:01:14 -0500

    Oops, it would help if I added the search functionality to tachyondecay.net!
    Also a few quality of life tweaks for result display.

--- lemonade_soapbox/views/frontend.py
+++ lemonade_soapbox/views/frontend.py
@@ -1,4 +1,7 @@
-from flask import render_template
+from flask import render_template, request, url_for
+from flask_sqlalchemy import Pagination
+from whoosh.query import Term as whoosh_term
+
 from lemonade_soapbox.models import Article, Review
 from lemonade_soapbox.helpers import Blueprint
 
@@ -23,7 +26,49 @@ def index():
 
 @bp.route('/search/')
 def search():
-    return ''
+    """Searching, obviously."""
+    q = request.args.get('q')
+    page = request.args.get('page', 1, int)
+    page_title = 'Search'
+    reviews = None
+    mode = None
+    if q:
+        search_params = {
+            'pagenum': page,
+            'pagelen': 20,
+            'filter': whoosh_term('status', 'published'),  # Only return published posts
+        }
+
+        # If query has no modifiers then search the title only
+        # split_q = q.split(" ")
+        # if split_q and ":" not in split_q[0]:
+        #     mode = 'title'
+        #     search_params['fields'] = 'title'
+
+        results = Article.search(q, **search_params)
+        # current_app.logger.debug(results)
+        if results is not None and results['query'] is not None:
+            articles = Pagination(
+                None,
+                page=page,
+                per_page=20,
+                total=results['total'],
+                items=results['query'].all(),
+            )
+            page_title = (
+                f"{results['total']} article{'s' if results['total'] > 1 else ''} found"
+            )
+        else:
+            page_title = "No reviews found"
+    return render_template(
+        'blog/views/article_list.html',
+        cover=url_for(
+            '.static',
+            filename='images/layout/header_bg/regine-tholen-pAs8r_VZEWc-unsplash.jpg',
+        ),
+        page_title=page_title,
+        articles=articles,
+    )
 
 
 @bp.route('/about/')
